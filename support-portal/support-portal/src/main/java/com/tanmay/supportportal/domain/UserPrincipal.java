76fdedbac004d5a5764c1c5449a1bc1b97804f81
modhtanmay <modhtanmay@gmail.com>
2021-01-06 12:07:03 +0530

    created UserPrincipal class for security: Domain Package

---
+++ support-portal/support-portal/src/main/java/com/tanmay/supportportal/domain/UserPrincipal.java
@@ -0,0 +1,66 @@
+package com.tanmay.supportportal.domain;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import static java.util.Arrays.stream;
+
+@Data
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails{ 
+	
+// a UserDetails object that represents the currently logged-in user in a Spring Boot application with Spring Security. 
+// Then you can display email, username, first name, last name, full name, assigned roles, any user’s information in the view
+
+	private User user;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return stream(this.user.getAuthorities()).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return this.user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.user.getUserName();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return this.user.isNotLocked();
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return this.user.isActive();
+	}
+
+}
