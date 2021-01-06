268bcde73c817c68de276c1ff7eca84ecce63e2e
modhtanmay <modhtanmay@gmail.com>
2021-01-06 22:00:45 +0530

    JWT Token Provider : Added Method getAuthorities

--- support-portal/support-portal/src/main/java/com/tanmay/supportportal/utility/JWTTokanProvider.java
+++ support-portal/support-portal/src/main/java/com/tanmay/supportportal/utility/JWTTokanProvider.java
@@ -1,18 +1,23 @@
 package com.tanmay.supportportal.utility;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.auth0.jwt.JWT;
 import com.tanmay.supportportal.constant.SecurityConstant;
 import com.tanmay.supportportal.domain.UserPrincipal;
 
-
+import static java.util.Arrays.stream;
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+
 public class JWTTokanProvider {
 
 	@Value("${jwt.secret}")
@@ -27,6 +32,15 @@ public class JWTTokanProvider {
 						   .sign(HMAC512(secret.getBytes()));
 	}
 
+	public List<GrantedAuthority> getAuthorities(String token){
+		String[] claims = getClaimsFromToken(token);
+		return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+	}
+	
+	private String[] getClaimsFromToken(String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
 		// TODO Auto-generated method stub
