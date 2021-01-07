da23bfe0343501f381e7fadbcad3e94ffa496080
modhtanmay <modhtanmay@gmail.com>
2021-01-07 10:08:24 +0530

    JWT Token Provider : Added Methods isTokenValid and isTokenExpired

--- support-portal/support-portal/src/main/java/com/tanmay/supportportal/utility/JWTTokanProvider.java
+++ support-portal/support-portal/src/main/java/com/tanmay/supportportal/utility/JWTTokanProvider.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -53,8 +54,17 @@ public class JWTTokanProvider {
 		return usernamePasswordAuthenticationToken;
 	}
 	
+	public boolean isTokenValid(String username,String token) {
+		JWTVerifier verifier = getJWTVerifier();
+		return StringUtils.isNotEmpty(username) && !isTokenExpired(verifier,token);
+	}
 	
-	
+	private boolean isTokenExpired(JWTVerifier verifier, String token) {
+		// TODO Auto-generated method stub
+		Date expiration = verifier.verify(token).getExpiresAt();
+		return expiration.before(new Date());
+	}
+
 	private String[] getClaimsFromToken(String token) {
 		// TODO Auto-generated method stub
 		JWTVerifier verifier = getJWTVerifier();
