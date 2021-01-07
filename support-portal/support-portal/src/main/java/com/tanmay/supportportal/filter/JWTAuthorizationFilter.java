443352e9f5d474c8eb635a18101a5b6910ccdaf6
modhtanmay <modhtanmay@gmail.com>
2021-01-08 02:39:28 +0530

    JWT Authorization Filter : updated doFilterInternal method

--- support-portal/support-portal/src/main/java/com/tanmay/supportportal/filter/JWTAuthorizationFilter.java
+++ support-portal/support-portal/src/main/java/com/tanmay/supportportal/filter/JWTAuthorizationFilter.java
@@ -1,5 +1,61 @@
 package com.tanmay.supportportal.filter;
 
-public class JWTAuthorizationFilter {
+import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.tanmay.supportportal.constant.SecurityConstant;
+import com.tanmay.supportportal.utility.JWTTokenProvider;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Component
+public class JWTAuthorizationFilter extends OncePerRequestFilter{
+	// Filter is used to stop users who are not Authenticated or if they have a bad Token to access the application
+	// so when request comes into application it will go through series of filters such as if token is valid,is the username correct and we can also 
+	// either say reject the request or accept the request and process it etc...
+
+	private JWTTokenProvider jwtTokenProvider;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		// check user,token is valid or not
+		if(request.getMethod().equalsIgnoreCase(SecurityConstant.OPTIONS_HTTP_METHOD)) {
+			response.setStatus(HttpStatus.OK.value());
+		} else {
+			String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+			if(authorizationHeader == null || authorizationHeader.startsWith(SecurityConstant.TOKEN_PREFIX)) {
+				filterChain.doFilter(request, response);
+				return;
+			}
+			String token = authorizationHeader.substring(SecurityConstant.TOKEN_PREFIX.length());
+			String username = jwtTokenProvider.getSubject(token);
+			if(jwtTokenProvider.isTokenValid(username, token) && SecurityContextHolder.getContext().getAuthentication() == null){
+				List<GrantedAuthority> authorities = jwtTokenProvider.getAuthorities(token);
+				Authentication authentication = jwtTokenProvider.getAuthentication(username, authorities, request);
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+			} else {
+				SecurityContextHolder.clearContext();
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
 }
