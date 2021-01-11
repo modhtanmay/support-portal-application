6c510af6f9a6161ce671717115ed25030cdaf997
modhtanmay <modhtanmay@gmail.com>
2021-01-11 13:42:37 +0530

    updated HttpResponse class

--- support-portal/support-portal/src/main/java/com/tanmay/supportportal/domain/HttpResponse.java
+++ support-portal/support-portal/src/main/java/com/tanmay/supportportal/domain/HttpResponse.java
@@ -4,6 +4,9 @@ import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +15,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class HttpResponse {  // this is response that we sent to user everytime
 	
+	@JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "MM-dd-yyyy hh:mm:ss",timezone = "America/New_York")
 	private Date timeStamp;
 	private int httpStatusCode;  // 200,201,400,500
 	private HttpStatus httpStatus;
