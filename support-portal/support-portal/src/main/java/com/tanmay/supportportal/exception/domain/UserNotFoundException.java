d16240c4357cd992a943d4d9b6ba397be61b98b0
modhtanmay <modhtanmay@gmail.com>
2021-01-11 15:59:18 +0530

    Added UserNotFoundException class : exception.domain package

---
+++ support-portal/support-portal/src/main/java/com/tanmay/supportportal/exception/domain/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.tanmay.supportportal.exception.domain;
+
+public class UserNotFoundException extends Exception {
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
