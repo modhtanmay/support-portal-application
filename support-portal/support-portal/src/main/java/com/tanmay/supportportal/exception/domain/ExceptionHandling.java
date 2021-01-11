75517a19e8044ba774c8dc3f024dede62d2ed25f
modhtanmay <modhtanmay@gmail.com>
2021-01-11 16:22:46 +0530

    Added ExceptionHandling class : exception.domain package

---
+++ support-portal/support-portal/src/main/java/com/tanmay/supportportal/exception/domain/ExceptionHandling.java
@@ -0,0 +1,11 @@
+package com.tanmay.supportportal.exception.domain;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandling {
+
+	// this class will act like controller to handle exception.
+	
+	
+}
