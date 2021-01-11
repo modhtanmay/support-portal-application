bfee6ded60eef18b33b4ab7431adb3d1045ccf58
modhtanmay <modhtanmay@gmail.com>
2021-01-11 15:54:55 +0530

    Added EmailNotFoundException class : exception.domain package

---
+++ support-portal/support-portal/src/main/java/com/tanmay/supportportal/exception/domain/EmailNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tanmay.supportportal.exception.domain;
+
+public class EmailNotFoundException extends Exception {
+	
+	public EmailNotFoundException(String message) {
+		super(message);
+	}
+
+}
