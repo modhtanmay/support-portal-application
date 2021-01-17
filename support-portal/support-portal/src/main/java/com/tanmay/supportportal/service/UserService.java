6fe032090ec55b048a3c549c2c84945926d942ac
modhtanmay <modhtanmay@gmail.com>
2021-01-17 17:14:13 +0530

    methods added to UserService Interface : service package

--- support-portal/support-portal/src/main/java/com/tanmay/supportportal/service/UserService.java
+++ support-portal/support-portal/src/main/java/com/tanmay/supportportal/service/UserService.java
@@ -1,5 +1,16 @@
 package com.tanmay.supportportal.service;
 
+import java.util.List;
+
+import com.tanmay.supportportal.domain.User;
+
 public interface UserService {
 
+	User register(String firstName, String lastName, String userName, String email);
+	
+	List<User> getUsers();
+	
+	User findUserByUserName(String userName);
+
+	User findUserByEmail(String email);
 }
